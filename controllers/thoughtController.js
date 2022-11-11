f76cb82c485e4e7c6f8af316ca181de5d0e70294
Rjewell859 <66131189+Rjewell859@users.noreply.github.com>
2022-11-11 15:12:35 -0500

    All created routes working so far

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -11,20 +11,30 @@ module.exports = {
   getSingleThought(req, res) {
     Thought.findOne({ _id: req.params.thoughtId })
       .select('-__v')
-      .lean()
-      .then(async (thought) =>
-        !thought ?
-        res.status(404).json({
-          message: 'There is no thought with that ID'
-        }) :
-        res.json({
-          thought,
-        }))
-      .catch((err) => {
-        console.log(err);
-        return res.status(500).json(err);
-      });
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'No thought with that ID' })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
   },
+  // getSingleThought(req, res) {
+  //   Thought.findOne({ _id: req.params.thoughtId })
+  //     .select('-__v')
+  //     .lean()
+  //     .then(async (thought) =>
+  //       !thought ?
+  //       res.status(404).json({
+  //         message: 'There is no thought with that ID'
+  //       }) :
+  //       res.json({
+  //         thought,
+  //       }))
+  //     .catch((err) => {
+  //       console.log(err);
+  //       return res.status(500).json(err);
+  //     });
+  // },
   // Create a thought
   createThought(req, res) {
     Thought.create(req.body)
--- controllers/userController.js
+++ controllers/userController.js
@@ -50,7 +50,20 @@ module.exports = {
       .then((user) => res.json(user))
       .catch((err) => res.status(500).json(err));
   },
-  // Delete a student and remove them from the course
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with this id!' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
   deleteUser(req, res) {
     User.findOneAndRemove({
         _id: req.params.userId
